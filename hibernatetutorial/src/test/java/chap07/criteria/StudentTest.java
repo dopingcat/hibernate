d8551f126b4264a652a9a3b1c582ec57b4db73aa
Vwan <dopingcat@nate.com>
2016-01-15 13:39:08 +0900

    add test method about namedQuery & nativeQuery & exampleQuery

--- hibernatetutorial/src/test/java/chap07/criteria/StudentTest.java
+++ hibernatetutorial/src/test/java/chap07/criteria/StudentTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.assertEquals;
 import java.util.List;
 
 import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
@@ -72,20 +74,74 @@ public class StudentTest {
 		Criteria criteria = session.createCriteria(Student.class);
 		criteria.add(Restrictions.or(Restrictions.between("age", 21, 25), Restrictions.between("kor", 70, 90)));
 		List<Student> list = criteria.list();
+		session.getTransaction().commit();
+
 		System.err.println("\n[나이가 21에서 25사이 이거나, 국어 점수가 70점 에서 90 사이]");
 		list.forEach(student -> System.out.println(student));
-		session.getTransaction().commit();
 	}
 
 	// Projection
 	// Doc : https://docs.jboss.org/hibernate/orm/3.6/javadocs/org/hibernate/criterion/Projections.html
-	@Test
+	//@Test
 	public void projection() throws Exception {
 		Session session = factory.getCurrentSession();
 		session.beginTransaction();
 		Criteria criteria = session.createCriteria(Student.class);
 		double avg = (double)criteria.setProjection(Projections.avg("kor")).uniqueResult();
-		System.err.println("\n[kor score avg] : " + avg);
 		session.getTransaction().commit();
+
+		System.err.println("\n[kor score avg] : " + avg);
+	}
+
+	// NamedQuery : 특정 쿼리가 많이 쓰일 때 Entity 클래스에 선언
+	// Ref : http://www.youtube.com/watch?v=o_P-p2b_k6w&index=28&list=PL5757A5DB24A40BDC
+	//@Test
+	public void namedQuery() throws Exception {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		Query query = session.getNamedQuery("FindByAge");
+		query.setInteger(0, 25);
+		List<Student> list = query.list();
+		session.getTransaction().commit();
+
+		System.err.println("\n[나이가 25 이상인 학생]");
+		list.forEach(student -> System.out.println(student));
+	}
+
+	// Native Query : DBMS 자체의 Native 쿼리(함수)를 쓸 때
+	// Ref : http://www.mkyong.com/hibernate/hibernate-native-sql-queries-examples/
+	//@Test
+	public void nativeQuery() throws Exception {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+		// query for derby
+		Query query = session.createSQLQuery("select * from \"USER\".\"STUDENT\"").addEntity(Student.class);
+		List<Student> list = query.list();
+		session.getTransaction().commit();
+
+		System.err.println("\n[순수 쿼리로 뽑은 리스트]");
+		list.forEach(student -> System.out.println(student));
+	}
+
+	// Example Query....왜 안돼는지 모름
+	// Ref : http://www.youtube.com/watch?v=2DXjdl8gzOo&list=PL5757A5DB24A40BDC&index=31
+	@Test
+	public void exampleQuery() throws Exception {
+		Session session = factory.getCurrentSession();
+		session.beginTransaction();
+
+		Student student = new Student();
+		student.setKor(90);
+
+		Example example = Example.create(student);
+
+		Criteria criteria = session.createCriteria(Student.class);
+		criteria.add(example);
+		List<Student> list = criteria.list();
+
+		session.getTransaction().commit();
+
+		System.err.println("\n[Example로 얻어진 객체]");
+		list.forEach(std -> System.out.println(std));
 	}
 }
